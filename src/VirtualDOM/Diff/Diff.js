05576d9ca500ceded3b43371f7972ea88c3be332
stephenliu <stephenlyao@yeah.net>
2016-11-23 23:53:05 +0800

    diff0.1

--- src/VirtualDOM/Diff/Diff.js
+++ src/VirtualDOM/Diff/Diff.js
@@ -11,8 +11,46 @@ export const Diff = (
   prevNode,
   nextNode,
   index = 0,
-  patches = {}
+  patches = []
 ) => {
   const currentPatches = []
+
+  if(prevNode === null && nextNode === null) return
+  if(prevNode === 'string' && nextNode === 'string'){
+    if(prevNode !== nextNode){
+      currentPatches.push({
+        type: TEXT,
+        content: nextNode
+      })
+    }
+  } else if(prevNode.tagName === nextNode.tagName && prevNode.key === nextNode.key){
+    // diff props
+    const propsPatches = diffProps(prevNode,nextNode)
+    if(propsPatches){
+      currentPatches.push({
+        type: PROPS,
+        props: propsPatches
+      })
+    }
+    //diff children
+    diffChildren(prevNode,nextNode,index,patches,currentPatches)
+  }else{
+    currentPatches.push({
+      type: REPLACE,
+      node: nextNode
+    })
+  }
+
+  if(currentPatches.length){
+    patches[index] = currentPatches
+  }
+
+  return patches
+}
+
+const diffProps = (
+  prevNode,
+  nextNode
+) => {
 
 }
